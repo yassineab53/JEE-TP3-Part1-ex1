a6e90206b339c118e1a651debefebf553ed19241
yassine4real <yssnabbou2002@gmail.com>
2024-11-25 17:13:09 +0100

    add Identification Servlet

---
+++ src/main/java/net/yassine/tp3/Identification.java
@@ -0,0 +1,27 @@
+package net.yassine.tp3;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author $ {USER}
+ **/
+public class Identification extends HttpServlet {
+
+    //private String message;
+
+    /*public void init() {
+        message = "Hello World!";
+    }*/
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+
+    }
+}
